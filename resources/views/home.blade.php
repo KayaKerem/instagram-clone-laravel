bd54a1040518dbab3f9daa9df8a035757912128d
Kerem Kaya <kerem@mgssoftware.net>
2022-06-04 11:49:41 +0300

    profile page designed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="row">
+    <div class="col-3 p-5">
+        <img src="/assets/profile-picture.jpg" class="rounded-circle">
+</div>
+    <div class="col-9 p-5 ">
+    <div><h1>kayaa.kerem</h1></div>
+<div class="d-flex">
+    <div class="pe-5" ><strong>2</strong> posts</div>
+    <div class="pe-5"><strong>1,046</strong> followers</div>
+    <div class="pe-5"><strong>762</strong> following</div>
+</div>
+<div class="pt-4">
+    <strong>Kerem Kaya</strong>
+</div>
+<div class="pt-1">YTU | CSE</div>
+<div><a href="www.youtube.com/c/KeremKayaa" style="text-decoration: none;">www.youtube.com/c/KeremKayaa</a></div>    
+   </div>
+</div>
+<div class="row pt-5">
+    <div class="col-4 d-flex">
+        <img src="/assets/post1.png" class="w-100 pe-3">
+        <img src="/assets/post1.png" class="w-100 pe-3">
+        <img src="/assets/post1.png" class="w-100 pe-3">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
     </div>
+</div>
 </div>
 @endsection
