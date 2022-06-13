065ec8d52e0999b849156b9ae60acfec69c18dab
Kerem Kaya <kerem@mgssoftware.net>
2022-06-13 10:18:56 +0300

    auth protection

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+        //if user isn't logged in, laravel routes the user to login page
+    }
  public function create(){
 
     return view('posts.create');
